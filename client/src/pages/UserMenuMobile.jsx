3197b91b2178a7454556da1ffba22c3e0284af47
ezhil0017 <ezhil0017@gmail.com>
2025-02-24 21:53:56 +0530

    user menu close code added

--- client/src/pages/UserMenuMobile.jsx
+++ client/src/pages/UserMenuMobile.jsx
@@ -5,7 +5,10 @@ import { IoClose } from 'react-icons/io5';
 const UserMenuMobile = () => {
   return (
     <section className='w-full h-full py-4 bg-white'>
-      <button className='block ml-auto text-neutral-800 w-fit'>
+      <button
+        className='block ml-auto text-neutral-800 w-fit'
+        onClick={() => window.history.back()}
+      >
         <IoClose size={25} />
       </button>
       <div className='container px-3 py-5 mx-auto'>
